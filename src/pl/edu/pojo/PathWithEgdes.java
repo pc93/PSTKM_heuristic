394cd63c8e89956ec2f18e90ea62c0752a26d569
Paweł <pczar93@gmail.com>
2017-01-14 14:54:27 +0100

    minFreeLoad of path

--- src/pl/edu/pojo/PathWithEgdes.java
+++ src/pl/edu/pojo/PathWithEgdes.java
@@ -6,11 +6,13 @@ public class PathWithEgdes {
 
 	private int index;
 	private List<Edge> edges;
+        private int minFreeLoad; //m = 10, path = (e1,e2), e1.load = 3, e2.load = 5 => maxFreeLoad = m - max(eX.load) = 10 - 5
 
 	public PathWithEgdes(int index, List<Edge> edges) {
 		super();
 		this.index = index;
 		this.edges = edges;
+                this.minFreeLoad = setMaxFreeLoad();
 	}
 
 	public int getIndex() {
@@ -28,6 +30,17 @@ public class PathWithEgdes {
 	public void setEdges(List<Edge> edges) {
 		this.edges = edges;
 	}
+        public int setMaxFreeLoad()
+        {
+            int maxFree = Config.MODULARITY;
+            for (Edge e : this.edges)
+            {
+                int x = (int)(Math.ceil(e.getCurrentLoad() % Config.MODULARITY)) * Config.MODULARITY - e.getCurrentLoad();
+                if (x < maxFree)
+                    maxFree = x;
+            }
+            return maxFree;
+        }
 
 	@Override
 	public String toString() {
